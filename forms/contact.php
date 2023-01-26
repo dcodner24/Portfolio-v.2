4ea43a3d17add1747882b495fcee864f4749071b
David Codner <dccodner24@gmail.com>
2023-01-25 23:04:48 -0600

    Configured SMTP

--- forms/contact.php
+++ forms/contact.php
@@ -24,14 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
+    'host' => 'email-smtp.us-east-2.amazonaws.com',
+    'username' => 'AKIAZXOAJORZ2C3TPPWV',
+    'password' => 'BI0KmZiTX9FnXSo2FYt1qoiEufYM0T/MqTbt6ZahLv49',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
